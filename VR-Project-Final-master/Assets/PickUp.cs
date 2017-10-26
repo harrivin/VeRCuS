02f236491531a9e9709aa52c7515cc49787ac05e
harrivin <harrivinn@gmail.com>
2017-10-26 07:40:32 -0400

    text kill enemy
    text1 text2

--- VR-Project-Final-master/Assets/PickUp.cs
+++ VR-Project-Final-master/Assets/PickUp.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class PickUp : MonoBehaviour {
     public int pickupcount;
@@ -11,6 +13,10 @@ public class PickUp : MonoBehaviour {
     public GameObject sphere4;
     public GameObject sphere5;
     public GameObject sphere6;
+    public GameObject text1;
+    public GameObject text2;
+    Text txt1;
+    Text txt2;
 
     // Use this for initialization
     void Start () {
@@ -22,6 +28,10 @@ public class PickUp : MonoBehaviour {
         sphere4 = GameObject.Find("PickUp/Sphere4");
         sphere5 = GameObject.Find("PickUp/Sphere5");
         sphere6 = GameObject.Find("PickUp/Sphere6");
+        text1 = GameObject.Find("FPSController/Canvas/Text1");
+        text2 = GameObject.Find("FPSController/Canvas/Text2");
+        txt1 = text1.GetComponent<Text>();
+        txt2 = text2.GetComponent<Text>();
     }
 
     // Update is called once per frame
@@ -34,51 +44,79 @@ public class PickUp : MonoBehaviour {
         if (other.gameObject==sphere)
         {
             sphere.SetActive(false);
+            txt1.text = "Kill 0";
+            txt2.text = "to get full health";
             pickupcount++;
+            StartCoroutine(waitTextSeconds());
             Debug.Log(pickupcount);
             Debug.Log("sphere");
+            StopCoroutine(waitTextSeconds());
         }
         else if (other.gameObject==sphere1)
         {
             sphere1.SetActive(false);
+            txt1.text = "Kill 1";
+            txt2.text = "to get full health";
             pickupcount++;
+            StartCoroutine(waitTextSeconds());
             Debug.Log(pickupcount);
             Debug.Log("sphere1");
+            StopCoroutine(waitTextSeconds());
         }
         else if (other.gameObject==sphere2)
         {
             sphere2.SetActive(false);
+            txt1.text = "Kill 2";
+            txt2.text = "to get full health";
             pickupcount++;
+            StartCoroutine(waitTextSeconds());
             Debug.Log(pickupcount);
             Debug.Log("sphere2");
+            StopCoroutine(waitTextSeconds());
         }
         else if (other.gameObject==sphere3)
         {
             sphere3.SetActive(false);
+            txt1.text = "Kill 3";
+            txt2.text = "to get full health";
             pickupcount++;
+            StartCoroutine(waitTextSeconds());
             Debug.Log(pickupcount);
             Debug.Log("sphere3");
+            StopCoroutine(waitTextSeconds());
         }
         else if (other.gameObject==sphere4)
         {
             sphere4.SetActive(false);
+            txt1.text = "Kill 4";
+            txt2.text = "to get full health";
             pickupcount++;
+            StartCoroutine(waitTextSeconds());
             Debug.Log(pickupcount);
             Debug.Log("sphere4");
+            StopCoroutine(waitTextSeconds());
         }
         else if (other.gameObject==sphere5)
         {
             sphere5.SetActive(false);
+            txt1.text = "Kill 5";
+            txt2.text = "to get full health";
             pickupcount++;
+            StartCoroutine(waitTextSeconds());
             Debug.Log(pickupcount);
             Debug.Log("sphere5");
+            StopCoroutine(waitTextSeconds());
         }
         else if (other.gameObject==sphere6)
         {
             sphere6.SetActive(false);
+            txt1.text = "Kill 6";
+            txt2.text = "to get full health";
             pickupcount++;
+            StartCoroutine(waitTextSeconds());
             Debug.Log(pickupcount);
             Debug.Log("sphere6");
+            StopCoroutine(waitTextSeconds());
         }
         if (pickupcount > 6)
         {
@@ -90,5 +128,11 @@ public class PickUp : MonoBehaviour {
     {
         yield return new WaitForSeconds(5);
         Application.LoadLevel("VR Scene1");
+    }
+    IEnumerator waitTextSeconds()
+    {
+        yield return new WaitForSeconds(5);
+        txt1.text = "";
+        txt2.text = "";
     }
 }
---
+++ VR-Project-Final-master/Assets/text1.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class text1 : MonoBehaviour
+{
+    public float time = 5; //Seconds to read the text
+    Text txt;
+
+
+    IEnumerator Start()
+    {
+
+        txt = gameObject.GetComponent<Text>();
+
+        yield return new WaitForSeconds(time);
+        txt.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
