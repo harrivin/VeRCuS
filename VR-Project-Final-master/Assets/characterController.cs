80ac7be74794ea3877e12cc9a672b68c0114c80c
harrivin <harrivinn@gmail.com>
2017-10-28 01:01:04 -0400

    wp
    way points

---
+++ VR-Project-Final-master/Assets/characterController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class characterController : MonoBehaviour {
+    Animator anim;
+    public float speed = 10.0f;
+    public float roationSpeed = 100.0f;
+    public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.Find("FPSController/Player"); 
+        anim = player.GetComponent<Animator>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        float translation = Input.GetAxis("Vertical") * speed;
+        float straffe = Input.GetAxis("Horizontal") * speed;
+        translation *= Time.deltaTime;
+        straffe *= Time.deltaTime;
+
+        transform.Translate(straffe, 0, translation);
+        if (Input.GetButton("Fire1"))
+        {
+            anim.SetBool("isAttacking", true);
+        }
+        else
+        {
+            anim.SetBool("isAttacking", false);
+        }
+
+        if (translation != 0)
+        {
+            anim.SetBool("isWalking", true);
+            anim.SetBool("isIdle", false);
+        }
+        else
+        {
+            anim.SetBool("isWalking", false);
+            anim.SetBool("isIdle", true);
+        }
+
+    }
+}
--- VR-Project-Final-master/Assets/chase.cs
+++ VR-Project-Final-master/Assets/chase.cs
@@ -7,7 +7,14 @@ public class chase : MonoBehaviour {
     public Transform player;
     public Transform head;
     Animator anim;
-    bool pursuing = false;
+    //bool pursuing = false;
+
+    string state = "patrol";
+    public GameObject[] waypoints;
+    int currentWP = 0;
+    public float rotspeed = 0.2f;
+    public float speed = 1.5f;
+    float accuracyWP = 5.0f;
 
 	// Use this for initialization
 	void Start () {
@@ -19,18 +26,36 @@ public class chase : MonoBehaviour {
     {
         Vector3 direction = player.position - this.transform.position;
         direction.y = 0;
-
         float angle = Vector3.Angle(direction, head.up);
 
-        if (Vector3.Distance(player.position, this.transform.position) < 10 && (angle < 30 || pursuing))
+        if(state=="patrol" && waypoints.Length > 0)
         {
-            pursuing = true;
-            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), 0.1f);
-
             anim.SetBool("isIdle", false);
+            anim.SetBool("isWalking", true);
+            if(Vector3.Distance(waypoints[currentWP].transform.position, transform.position) < accuracyWP)
+            {
+                currentWP = Random.Range(0, waypoints.Length);
+                //currentWP++;
+                //if(currentWP >= waypoints.Length)
+                //{
+                //    currentWP = 0;
+                //}
+            }
+            direction = waypoints[currentWP].transform.position - transform.position;
+            this.transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), rotspeed * Time.deltaTime);
+            this.transform.Translate(0, 0, Time.deltaTime * speed);
+        }
+
+        if (Vector3.Distance(player.position, this.transform.position) < 10 && (angle < 30 || state=="pursuing"))
+        {
+            state = "pursuing";
+
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), rotspeed * Time.deltaTime);
+
+            //anim.SetBool("isIdle", false);
             if (direction.magnitude > 3)
             {
-                this.transform.Translate(0, 0, 0.05f);
+                this.transform.Translate(0, 0, Time.deltaTime * speed);
                 anim.SetBool("isWalking", true);
                 anim.SetBool("isAttacking", false);
             }
@@ -42,10 +67,10 @@ public class chase : MonoBehaviour {
         }
         else
         {
-            anim.SetBool("isIdle", true);
-            anim.SetBool("isWalking", false);
+            //anim.SetBool("isIdle", true);
+            anim.SetBool("isWalking", true);
             anim.SetBool("isAttacking", false);
-            pursuing = false;
+            state = "patrol";
        
         }
     }
