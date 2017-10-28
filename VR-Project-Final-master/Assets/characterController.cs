64c071848b74cd8ba94ed17d5f0cf906934dc06c
harrivin <harrivinn@gmail.com>
2017-10-28 02:32:26 -0400

    imperfect check working ani for fps
    imperfect check working ani for fps

--- VR-Project-Final-master/Assets/characterController.cs
+++ VR-Project-Final-master/Assets/characterController.cs
@@ -3,16 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class characterController : MonoBehaviour {
-    Animator anim;
+    Animator anim1;
     public float speed = 10.0f;
     public float roationSpeed = 100.0f;
-    public GameObject player;
 
-	// Use this for initialization
-	void Start () {
-        player = GameObject.Find("FPSController/Player"); 
-        anim = player.GetComponent<Animator>();
-	}
+   // public GameObject player;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        //player = GameObject.Find("FPSController/Player");
+
+        anim1 = GetComponent<Animator>();
+    }
 
     // Update is called once per frame
     void Update()
@@ -25,22 +29,23 @@ public class characterController : MonoBehaviour {
         transform.Translate(straffe, 0, translation);
         if (Input.GetButton("Fire1"))
         {
-            anim.SetBool("isAttacking", true);
+            anim1.SetBool("isAttacking", true);
+           
         }
         else
         {
-            anim.SetBool("isAttacking", false);
+            anim1.SetBool("isAttacking", false);
         }
 
         if (translation != 0)
         {
-            anim.SetBool("isWalking", true);
-            anim.SetBool("isIdle", false);
+            anim1.SetBool("isWalking", true);
+            anim1.SetBool("isIdle", false);
         }
         else
         {
-            anim.SetBool("isWalking", false);
-            anim.SetBool("isIdle", true);
+            anim1.SetBool("isWalking", false);
+            anim1.SetBool("isIdle", true);
         }
 
     }
