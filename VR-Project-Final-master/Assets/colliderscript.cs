3f5ae4990d3192ed7087b0106841e00e5eef4cd9
harrivin <harrivinn@gmail.com>
2017-11-15 17:46:35 -0500

    level 2 near perfect
    level 2 near perfect

--- VR-Project-Final-master/Assets/colliderscript.cs
+++ VR-Project-Final-master/Assets/colliderscript.cs
@@ -3,20 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class colliderscript : MonoBehaviour {
+public class colliderscript : MonoBehaviour
+{
     public string opponent1;
     public int k = 0;
-    
+
     public Button submitbutton1;
     public GameObject[] cubes;
 
     // Use this for initialization
-    void Start() {
+    void Start()
+    {
         submitbutton1.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
-    void Update() {
+    void Update()
+    {
         //if (j == 1)
         //{
         //    submitbutton1.gameObject.SetActive(false);
@@ -25,7 +28,7 @@ public class colliderscript : MonoBehaviour {
         //{
         //    submitbutton1.gameObject.SetActive(true);
         //}
-       
+
     }
 
     void OnTriggerEnter(Collider other)
@@ -49,5 +52,22 @@ public class colliderscript : MonoBehaviour {
 
         }
 
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag != opponent1) return;
+        k--;
+        Debug.Log(k);
+        if (cubes[0].GetComponent<colliderscript>().k == 1 && cubes[1].GetComponent<colliderscript>().k == 1 && cubes[2].GetComponent<colliderscript>().k == 1 && cubes[3].GetComponent<colliderscript>().k == 1 && cubes[4].GetComponent<colliderscript>().k == 1 && cubes[5].GetComponent<colliderscript>().k == 1 && cubes[6].GetComponent<colliderscript>().k == 1)
+        {
+
+            submitbutton1.gameObject.SetActive(true);
+        }
+        else
+        {
+            submitbutton1.gameObject.SetActive(false);
+
+        }
     }
 }
