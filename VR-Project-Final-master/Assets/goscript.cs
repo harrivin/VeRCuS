870039130931ecb179d3c1ac6d7ccb3106252905
harrivin <harrivinn@gmail.com>
2017-11-14 22:42:41 -0500

    go scene
    go scene

---
+++ VR-Project-Final-master/Assets/goscript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class goscript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButton("Fire1"))
+        {
+            Application.LoadLevel("VR Scene");
+
+        }
+        else if(Input.GetButton("Fire2"))
+        {
+            Application.Quit();
+        }
+    }
+}
